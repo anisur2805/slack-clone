49d9516bfef658b9d626b4330ce1543c786d70eb
anisur2805 <anisur2805@gmail.com>
2021-07-19 00:51:26 +0600

    connect rooms with firebase and show channels

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDVVyB7-7oxFzZbB21OBTmZnpq7oAB5WrQ",
     authDomain: "slack-clone-bad04.firebaseapp.com",
@@ -8,3 +9,10 @@ const firebaseConfig = {
     appId: "1:371992911846:web:5856e3cdfc7b008dff8027",
     measurementId: "G-FDZZ42VFB0",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {db, auth, provider}
